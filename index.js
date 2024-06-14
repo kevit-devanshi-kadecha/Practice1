20b6ed0e2a326849c0afe0f4e35da74db592ab9e
devanshik <devanshi.kadecha@kevit.io>
2024-06-14 16:16:47 +0530

    added route for random user count

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', function (req, res) {
    res.send('Hello GET');
 })
 
+app.get('/', function (req, res) {
+    console.log("Got a GET request for the homepage");
+    const userCount = Math.ceil(Math.random() * 100);
+    res.send(`Hello GET. Users are: ${userCount}`);
+ })
 
 // This responds a POST request for the homepage
 app.post('/', function (req, res) {
